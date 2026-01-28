f663664fbe480bbf3e534ae68fee56aa9732e4ef
Aferpal <127872626+Aferpal@users.noreply.github.com>
2026-01-28 20:45:27 +0100

    Feature: Adding Vector2 class (#1)
    Added class for bidimensional vector, including all arithmetic operands

---
+++ include/Vector2.hpp
@@ -0,0 +1,55 @@
+#ifndef _VECTOR_2_
+#define _VECTOR_2_
+
+class Vector2{
+    private:
+        float m_x, m_y;
+    public:
+
+        // CONSTRUCTORS
+
+        Vector2();
+        Vector2(float);
+        Vector2(float, float);
+
+        Vector2(const Vector2&) = default;
+        Vector2(Vector2&&) = default;
+
+
+        // MOVE OPERATORS
+
+        Vector2& operator=(const Vector2&);
+        Vector2& operator=(Vector2&&);
+
+
+        // ARITHMETIC OPERATORS
+
+        Vector2 operator+(const Vector2&) const;
+        Vector2 operator+(Vector2&&) const;
+
+        Vector2& operator+=(const Vector2&);
+        Vector2& operator+=(Vector2&&);
+
+        Vector2 operator-(const Vector2&) const;
+        Vector2 operator-(Vector2&&) const;
+
+        Vector2& operator-=(const Vector2&);
+        Vector2& operator-=(Vector2&&);
+
+        Vector2 operator*(float) const;
+        Vector2& operator*=(float);
+
+        // EQUAL OPERATOR
+
+        bool operator==(const Vector2&) const;
+        bool operator==(Vector2&&) const;
+
+        // GETTERS
+
+        inline float getX() const;
+
+        inline float getY() const;
+
+};
+
+#endif
---
+++ src/Vector2.cpp
@@ -0,0 +1,155 @@
+#include<Vector2.hpp>
+
+
+// CONSTRUCTORS
+
+Vector2::Vector2(): Vector2(0, 0){}
+
+Vector2::Vector2(float value): Vector2(value, value){}
+
+Vector2::Vector2(float x, float y): m_x(x), m_y(y){}
+
+
+// MOVE OPERATORS
+
+Vector2& Vector2::operator=(const Vector2& other){
+
+    if( this == &other ){
+        return *this;
+    }
+
+    m_x = other.m_x;
+    m_y = other.m_y;
+
+    return *this;
+}
+
+Vector2& Vector2::operator=(Vector2&& other){
+
+    m_x = other.m_x;
+    m_y = other.m_y;
+
+    return *this;
+}
+
+
+
+// ARITHMETIC OPERATORS
+
+
+
+// operator +
+
+Vector2 Vector2::operator+(const Vector2& other) const {
+
+    return Vector2(m_x + other.m_x, m_y +  other.m_y);
+
+}
+
+Vector2 Vector2::operator+(Vector2&& other) const {
+
+    return Vector2(m_x + other.m_x, m_y +  other.m_y);
+    
+}
+
+//operator +=
+
+Vector2& Vector2::operator+=(const Vector2& other){
+
+    m_x = m_x + other.m_x;
+    m_y = m_y + other.m_y;
+
+    return *this;
+
+}
+
+Vector2& Vector2::operator+=(Vector2&& other){
+
+    m_x = m_x + other.m_x;
+    m_y = m_y + other.m_y;
+
+    return *this;
+
+}
+
+
+
+
+
+// operator -
+
+Vector2 Vector2::operator-(const Vector2& other) const {
+
+    return Vector2(m_x - other.m_x, m_y - other.m_y);
+
+}
+
+Vector2 Vector2::operator-(Vector2&& other) const {
+
+    return Vector2(m_x - other.m_x, m_y - other.m_y);
+    
+}
+
+// operator -=
+
+Vector2& Vector2::operator-=(const Vector2& other){
+
+    m_x = m_x - other.m_x;
+    m_y = m_y - other.m_y;
+
+    return *this;
+
+}
+
+Vector2& Vector2::operator-=(Vector2&& other){
+
+    m_x = m_x - other.m_x;
+    m_y = m_y - other.m_y;
+
+    return *this;
+
+}
+
+
+
+// operator *
+
+Vector2 Vector2::operator*(float factor) const {
+
+    return Vector2(m_x * factor, m_y * factor);
+
+}
+
+// operator *=
+
+Vector2& Vector2::operator*=(float factor){
+
+    m_x = m_x * factor;
+    m_y = m_y * factor;
+
+    return *this;
+
+}
+
+
+// operator ==
+
+bool Vector2::operator==(const Vector2& other) const{
+    return (m_x == other.m_x && m_y == other.m_y);
+}
+        
+bool Vector2::operator==(Vector2&& other) const{
+    return (m_x == other.m_x && m_y == other.m_y);
+}
+
+
+
+// GETTERS
+
+inline float Vector2::getX() const {
+    return m_x;
+}
+
+inline float Vector2::getY() const {
+    return m_y;
+}

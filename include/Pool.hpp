043357c26c43319abd57d50248f500d16418b4d3
Aferpal <fernandezpalomoabel@gmail.com>
2026-01-29 12:04:44 +0100

    Feature: Added Pool Container
    Added pool as a data structure to store components with an underlying sparse set.

---
+++ include/Pool.hpp
@@ -0,0 +1,103 @@
+#ifndef _POOL_
+#define _POOL_
+
+#include<vector>
+#include "types.hpp"
+#include<iostream>
+namespace afp{
+
+    template<typename T>
+    class Pool{
+
+        public:
+            Pool() = default;
+
+            Pool(const Pool&) = delete;
+
+            Pool(Pool&&) = delete;
+
+            bool contains(EntityID id){
+
+                return id < spare.size() && spare[id] < dense_id.size() && id == dense_id[spare[id]];
+
+            }
+
+            template<typename... Args>
+            T& insert(EntityID id, Args&& ... args){
+
+                // this means there have been new entities created, so we resize our spare vector
+                if( id >= spare.size() ){
+
+                    spare.resize(id + 1);
+
+                }
+                 
+                if( contains(id) ){ // in case there is actually an inserted element
+                    throw 0;                     // we return, for now the intended functionality is to IGNORE the insertion in this case
+                }
+
+                dense_id.emplace_back(id);
+
+                dense_components.emplace_back(std::forward<Args>(args)...);
+
+                spare[id] = dense_id.size() - 1;
+
+                return dense_components.back();
+
+            }
+
+            T& get(EntityID id){
+
+                if( !contains(id) ){ // in case there is actually an inserted element
+                    throw 0;         // we return, for now the intended functionality is to IGNORE the insertion in this case
+                }  
+
+                return dense_components[spare[id]];
+
+            }
+
+            const T& get(EntityID id) const {
+                
+                if( !contains(id) ){ // in case there is actually an inserted element
+                    throw 0;         // we return, for now the intended functionality is to IGNORE the insertion in this case
+                }  
+
+                return dense_components[spare[id]];
+
+            }
+
+            void erase(EntityID id){
+
+                if( contains(id) ){
+                    
+                    unsigned int index = spare[id];
+
+                    dense_components[index] = dense_components.back();
+
+                    dense_components.pop_back();
+
+                    dense_id[index] = dense_id.back();
+
+                    dense_id.pop_back();
+
+                    spare[dense_id[index]] = index;
+                    
+                }
+
+            }
+
+
+
+        private:
+
+            std::vector<T> dense_components;
+
+            std::vector<EntityID> dense_id;
+
+            std::vector<unsigned int> spare;
+
+    };
+
+}
+
+#endif
---
+++ include/types.hpp
@@ -0,0 +1,6 @@
+#ifndef _TYPES_
+#define _TYPES_
+
+using EntityID = unsigned int;
+
+#endif
